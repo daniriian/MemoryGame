63145e91b9d33a30bb2dfd6dade6ab6048f44698
DRT <daniriian@gmail.com>
2018-03-10 20:58:21 +0200

    add event listener for cards

--- js/app.js
+++ js/app.js
@@ -106,3 +106,23 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+//set up the listener event for cards
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function() {
+        if (!cards[i].classList.contains('open')) {
+            hitCounter++;
+            if (hitCounter === 1) {
+                //timer
+                timeInterval = setInterval(setTime, 1000);
+            }
+            showCard(i);
+            if (hitCounter % 2 === 1) {
+                firstCardIndex = i;
+            } else {
+                verifyMatch(i);
+                updateScore(hitCounter);
+            }
+        }
+    });
+}
