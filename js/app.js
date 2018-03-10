0a673a6ac329fce69e48491b83ba5db6eb8b46ad
DRT <daniriian@gmail.com>
2018-03-10 20:54:01 +0200

    declare all variables for js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+//Variable declaration
+let cardSymbols = []; //array to hold symbols
+let hitCounter = 0;
+let firstCardIndex = 0;
+let pairsToDiscover = 8;
+let timer = 0;
+let timeInterval;
+let mm = 0,
+    ss = 0,
+    starRating = 3;
+
+//restart button
+let rb = document.getElementById('restart_button');
+
+//cards
+let cards = document.querySelectorAll('.deck .card');
+
+//elapsed time
+let elapsedTime = document.querySelector('.timer');
+
+//
+let numOfMoves = document.querySelector('.moves');
+
+//stars in Star Rating
+let stars = document.querySelector('.stars');
+
+//total number of moves
+let m_totalMoves = document.getElementById('totalMoves');
+
+//total time
+let m_totalTime = document.getElementById('totalTime');
+
+//Modal Star rating
+let m_starRating = document.getElementById('starRating');
+
+//modat - Score tabel
+let modal = document.getElementById('modal');
+
 /*
  * Create a list that holds all of your cards
  */
@@ -12,7 +50,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
